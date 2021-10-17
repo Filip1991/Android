208a68a0b8ad44cfbd0936377688ac08f5e110f9
Filip1991 <filip.victor@yahoo.com>
2021-10-17 22:11:11 +0300

    Add files via upload

---
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.SearchView;
+
+import static android.util.Log.v;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buton;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.v("lifecycle", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        v("Lifecycle", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        v("Lifecycle", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        v("Lifecycle", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        v("Lifecycle", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        v("Lifecycle", "onDestroy");
+    }
+}
